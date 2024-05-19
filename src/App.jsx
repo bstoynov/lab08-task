2be07b7b4a5cd3ba358b42a917bd3fd500f29963
Bobby Stoynov <bobbystoynov@gmail.com>
2024-05-18 20:35:47 -0700

    serve data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
 import List from "./components/List/List";
 
 export default function App() {
-  return (
-    <main>
-      <List />
-    </main>
-  );
+  const [threads, setThreads] = useState(null);
+
+  useEffect(() => {
+    async function fetchThreads() {
+      const response = await fetch("http://localhost:3000/threads");
+      const result = await response.json();
+      console.log(result);
+      return result;
+    }
+
+    if (!threads) {
+      fetchThreads().then(setThreads);
+    }
+  }, [threads]);
+  return <main>{threads && <List />}</main>;
 }
