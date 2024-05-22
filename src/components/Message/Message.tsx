e64a3a613dd74a2e290d547e03190b9270a70927
Bobby Stoynov <bobbystoynov@gmail.com>
2024-05-22 16:13:46 -0700

    update colors

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -19,7 +19,6 @@ export default function Message({
   rating,
 }: MessageProps) {
   function getRatingClass(rating) {
-    // console.log(rating);
     switch (rating) {
       case Rating.High:
         return "rating__high";
